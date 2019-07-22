4576f747a445259976ce97bede94b6717a248d66
Taichi <taichi@asaichi.co.jp>
2019-07-22 22:08:30 +0900

    SeederとFactory

---
+++ src/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+//        DB::table('users')->insert([
+//            'name' => '中村太一',
+//            'email' => 'taichi@asaichi.co.jp',
+//            'password' => bcrypt('secret'),
+//        ]);
+//
+//        $user = new User();
+//        $user->name = '中村太一2';
+//        $user->email = 'taichi2@asaichi.co.jp';
+//        $user->password = bcrypt('secret');
+//        $user->save();
+
+        factory(User::class, 20)->create();
+    }
+}
